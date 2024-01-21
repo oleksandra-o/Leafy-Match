00a53c78e87c696504a5cf65954ee5f29e05e732
oleksandra-o <156704678+oleksandra-o@users.noreply.github.com>
2024-01-20 18:08:47 -0700

    new sign in page

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSignup() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('signupForm').style.display = 'block';
+}
+
+function showLogin() {
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('signupForm').style.display = 'none';
+}
