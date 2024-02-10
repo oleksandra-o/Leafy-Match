cf1c1c9b33772257175d686b5e51c8f531499ce2
oleksandra-o <156704678+oleksandra-o@users.noreply.github.com>
2024-02-09 20:48:53 -0700

    navbar

--- my-leafy-match/src/components/Navbar.js
+++ my-leafy-match/src/components/Navbar.js
@@ -1,48 +1,42 @@
-
-// In Navbar.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles.css';
-
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      {/* Bootstrap navbar*/}
-      
-    <div class="container">
-        <a class="navbar-brand" href="#">LEAFY MATCH</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <div className="container">
+        <Link className="navbar-brand" to="/">LEAFY MATCH</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="home.html">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="about.html">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="">Blog</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="signin.html"><i class="fas fa-user"></i> Sign In</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="cart.html"><i class="fas fa-shopping-cart"></i> Cart</a>
-                </li>
-            </ul>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav mx-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/signin"><i className="fas fa-user"></i> Sign In</Link>
+            </li>
+            {/* Update this link once you have a route for the cart */}
+            <li className="nav-item">
+              <Link className="nav-link" to="/cart"><i className="fas fa-shopping-cart"></i> Cart</Link>
+            </li>
+          </ul>
         </div>
-    </div>
-
+      </div>
     </nav>
   );
 };
 
 export default Navbar;
+
 
