dcc91ac74e173c83b978a2b2de578fa2a986ef8c
oleksandra-o <156704678+oleksandra-o@users.noreply.github.com>
2024-02-11 19:41:25 -0700

    updated nav bar

--- my-leafy-match/src/components/Navbar.js
+++ my-leafy-match/src/components/Navbar.js
@@ -1,39 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Navbar = () => {
+const NavigationBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <div className="container">
-        <Link className="navbar-brand" to="/">LEAFY MATCH</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mx-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/signin"><i className="fas fa-user"></i> Sign In</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/cart"><i className="fas fa-shopping-cart"></i> Cart</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top">
+      <Container>
+        <Navbar.Brand as={Link} to="/">LEAFY MATCH</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/signin"><i className="fas fa-user"></i> Sign In</Nav.Link>
+            <Nav.Link as={Link} to="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default NavigationBar;
